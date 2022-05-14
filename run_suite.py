99d841c7f9ce9768ee1a96ccb64ec35d99c39fdf
Xun <3020284554@qq.com>
2022-05-14 22:20:54 +0800

    time

--- run_suite.py
+++ run_suite.py
@@ -15,8 +15,8 @@ suite.addTest(unittest.makeSuite(approver))
 suite.addTest(unittest.makeSuite(trust))
 suite.addTest(unittest.makeSuite(tender))
 
-report_file = app.BASE_DRL + "/reports/report{}.html".format(time.strftime("%Y%m%d-%H%M%S"))
-
+# report_file = app.BASE_DRL + "/reports/report{}.html".format(time.strftime("%Y%m%d-%H%M%S"))
+report_file = app.BASE_DRL + "/reports/report.html"
 with open(report_file, mode='wb') as f:
     runner = HTMLTestRunner_PY3.HTMLTestRunner(f, title="P2P金融的接口测试报告", description="test")
 # 运行测试套件
